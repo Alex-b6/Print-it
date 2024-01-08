44f70660237512d8a3a51d28fa49e7c9f44547f5
AlexB6 <brou.alexandre.pro@gmail.com>
2024-01-08 09:36:21 +0100

    adding slider

--- assets/script.js
+++ assets/script.js
@@ -15,4 +15,68 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
+//on définit les constantes
+let position = 0;
+const numberOfSlide = slides.length;
+const left = document.querySelector('.arrow_left');
+const right = document.querySelector('.arrow_right');
+createSlider(position);
+createDots();
+updateDot();
+//création de la fonction carrousel
+function createSlider(position){
+	
+	const element = slides[position];
+	  //console.log(element);
+	  const img = document.querySelector(".slider-img");
+	  img.setAttribute("src" , "./assets/images/slideshow/"+ element.image);
+	  const p = document.querySelector(".slider-txt");
+	  p.innerHTML= element.tagLine;
+
+  updateDot();
+}
+//création de l'évenement "clic gauche"
+left.addEventListener("click", function() {
+	if (position == 0) {
+        position = numberOfSlide - 1;
+    }
+    else {
+        position--;
+    }
+        createSlider(position);
+});
+//création de l'évenement "clic droit"
+right.addEventListener("click", function () {
+    if (position == numberOfSlide - 1) {
+        position = 0;
+    } else {
+            position++;
+    }
+    createSlider(position);
+});
+//création des dots
+function createDots(){
+	const dots = document.querySelector(".dots");	
+   for (let index = 0; index < slides.length; index++) {
+	   // création de dot pour chaque element dans la boucle
+	   const dot= document.createElement("div");
+	   dot.setAttribute("class", "dot");
+	   dots.appendChild(dot);
+   } 
+}
+//MaJ des dots en fonction de l'image sélectionnée
+function updateDot() {
+ const listPoints = document.querySelectorAll(".dot");	
+  for (let index = 0; index < listPoints.length; index++) {
+  
+   const dot = listPoints[index];
+   if (index == position){
+	   dot.classList.add('dot_selected');		
+ }
+ else{
+   dot.classList.remove('dot_selected');	 
+ }
+   
+   }  
+}
